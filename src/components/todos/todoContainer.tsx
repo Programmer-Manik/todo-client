b878005b8c8a2afba6242be19d5d2872871ccb70
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-05 13:16:55 +0600

    Use Query In RTK Query

--- src/components/todos/todoContainer.tsx
+++ src/components/todos/todoContainer.tsx
@@ -6,6 +6,11 @@ import TodoFilter from "./todoFilter";
 
 const TodoContainer = () => {
   const {todos} = useAppSelector((state) => state.todos )
+  //from server
+  // const {data:todos,isLoading, isError} = useGetTodosQuery(undefined);
+  // if(isLoading){
+  //   return <p>Loading...</p>
+  // }
   return (
     <div>
       <div className="flex justify-between mb-10 ">
@@ -14,8 +19,9 @@ const TodoContainer = () => {
         <TodoFilter />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
-        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
           {todos.map((item) => <TodoCord {...item}/>)}
+          {/* {todos?.map((item) => <TodoCord {...item}/>)} */}
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
         </div>
           {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
           <p>There is no task pending</p>{' '}
